58d9f77576287869a7cbca90916340e76a8e7c20
RBocaling <rbocaling@qmdsi.com>
2025-02-02 16:21:48 +0800

    upload

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import fetch from "node-fetch";
 import fs from "fs";
+import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 import { extractTicker, userComment } from "./utils/openAiHelper.js";
@@ -12,7 +13,7 @@ puppeteer.use(StealthPlugin());
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 app.use(express.json());
-
+app.use(cors({ origin: "*" }));
 const PORT = process.env.PORT || 3000;
 const imagesFolder = path.join(__dirname, "savedImages");
 
